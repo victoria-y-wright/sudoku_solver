7dcc89aa745a6c0cab14c0968d165d276904b284
victoria-y-wright <victoria.y.wright+github.com@outlook.com>
2024-10-10 15:16:18 +0100

    update

--- python_script/modules/__init__.py
+++ python_script/modules/__init__.py
@@ -1,11 +1,11 @@
-from os import listdir
-from os.path import abspath, dirname, isfile, join
+# from os import listdir
+# from os.path import abspath, dirname, isfile, join
 
-init_path = abspath(__file__)
-init_dir = dirname(init_path)
+# init_path = abspath(__file__)
+# init_dir = dirname(init_path)
 
-py_files = [file_name.replace(".py", "") for file_name in listdir(init_dir) \
-           if isfile(join(init_dir, file_name)) and ".py" in file_name and not ".pyc" in file_name]
-py_files.remove("__init__")
+# py_files = [file_name.replace(".py", "") for file_name in listdir(init_dir) \
+#            if isfile(join(init_dir, file_name)) and ".py" in file_name and not ".pyc" in file_name]
+# py_files.remove("__init__")
 
-__all__ = py_files
+# __all__ = py_files
